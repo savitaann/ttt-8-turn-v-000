54b7f97e54254c5edc9eba719e08219fd72165bc
savita madray <savitamadray01@gmail.com>
2017-10-17 16:11:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,35 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} " "|" " #{board[7]} " "|" " #{board[8]} "
 end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+
+end
+
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
+       return false
+  elsif board[index] == nil
+          return false
+  else board[index] == "X" ||  board[index] == "O"
+        return true
+  end
+end
+
+def valid_move?(board, index)
+
+  if position_taken?(board, index) != true && index.between?(0, 8)
+    return true
+
+
+
+  end
+  end
